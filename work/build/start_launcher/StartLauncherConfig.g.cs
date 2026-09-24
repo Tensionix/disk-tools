5f38e0bedbe6d471088db9b7f712ebec2ec90dcb
Igor <ibabailov@gmail.com>
2026-09-25 01:23:03 +0600

    cleanup: remove files left over from Audion Disk Tools 4.0.1

--- work/build/start_launcher/StartLauncherConfig.g.cs
+++
@@ -1,6 +0,0 @@
-﻿internal static class StartLauncherConfig
-{
-    public const string AppName = "Audion Disk Tools";
-    public const string AppId = "Audion.Tools.Audion.Disk.Tools";
-    public const bool RequireAdministrator = true;
-}
--- work/start_launcher/StartElevated.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-chcp 65001 >nul
-setlocal EnableExtensions
-set "AUDION_APP_NAME=Audion Disk Tools"
-set "AUDION_APP_ID=Audion.Tools.Audion.Disk.Tools"
-set "AUDION_GUI_ELEVATE=1"
-set "AUDION_APP_ICON=E:\TOOLS\Audion Disk Tools\system_core\icons\app.ico"
-call "E:\TOOLS\Audion Disk Tools\launcher_gui.cmd"
-exit /b %ERRORLEVEL%
